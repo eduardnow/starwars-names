99a2ec143a7dec76ad832cf7799a099db45d56d9
Eduard Castro <eduard.cas07@gmail.com>
2020-04-22 19:54:52 -0500

    Add initial test configuration

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwras-names', () => {
+    it('should work!', () => {
+        expect(true).to.be.true;
+    });
+});
