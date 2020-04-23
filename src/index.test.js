c25873829486945f942f7dcbc2f7cf0d0ee75293
Eduard Castro <eduard.cas07@gmail.com>
2020-04-22 20:09:30 -0500

    Add unit tests

--- src/index.test.js
+++ src/index.test.js
@@ -5,4 +5,29 @@ describe('starwras-names', () => {
     it('should work!', () => {
         expect(true).to.be.true;
     });
+
+    describe('all', () => {
+        it('should be an array of string', () => {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every((item) => {
+                    return typeof item === 'string';
+                });
+            }
+        });
+
+        it('should contain `Luke Skywalker`', () => {
+            expect(starWars.all).to.include('Luke Skywalker');
+        });
+    });
+
+
+    describe('random', () => {
+        it('should return a random item from the starWars.all', () => {
+            var randomItem = starWars.random();
+
+            expect(starWars.all).to.include(randomItem);
+        });
+    });
 });
